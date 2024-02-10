f57b47d62a4c880b48c4480b90c688c0023439ba
Mercy-chemtai <chemtailodite@gmail.com>
2024-02-10 10:39:39 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+   var fullName ="Mercy Chemtai"
+       println(fullName)
+    var age = 22
+    println(age)
+    var phoneNumber = +254112580268
+    println(phoneNumber)
+    val citizen = "True"
+            println(citizen )
+    val nonCitizen = "False"
+            println(nonCitizen)
+
+}
